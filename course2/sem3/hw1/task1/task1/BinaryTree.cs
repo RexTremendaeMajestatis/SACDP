1e7f02667332fb514c32bdf790a2e2ce3ad6c2bb
Paul Kizhnerov <stigma-diabolicum@yandex.by>
2017-09-27 04:08:35 +0300

    Add & Find were created

--- course2/sem3/hw1/task1/task1/BinaryTree.cs
+++ course2/sem3/hw1/task1/task1/BinaryTree.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace task1
 {
-    class BinaryTree
+    public partial class BinaryTree<T>
+        where T : IComparable
     {
+        private Node<T> root;
+        
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public BinaryTree(T data)
+        { 
+            root = new Node<T>(data);
+        }
+        public void Add(T data)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(data);
+            }
+            else
+            {
+                Add(root, data);
+            }
+        }
+        public Node<T> Find(T data)
+        {
+            return Find(root, data);
+        }
+        void Add(Node<T> node, T data)
+        {
+            if (node.data.Equals(data))
+            {
+                return;
+            }
+            if (node.data.CompareTo(data) > 0)
+            {
+                if (node.right == null)
+                {
+                    node.right = new Node<T>(data);
+                }
+                else
+                {
+                    Add(node.right, data);
+                }
+            }
+            else
+            {
+                if (node.left == null)
+                {
+                    node.left = new Node<T>(data);
+                }
+                else
+                {
+                    Add(node.left, data);
+                }
+            }
+        }
+        Node<T> Find(Node<T> node, T data)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+            if (node.data.Equals(data))
+            {
+                return node;
+            }
+            if (node.data.CompareTo(data) > 0)
+            {
+                return Find(node.right, data);
+            }
+                return Find(node.left, data);
+            
+        }
     }
 }
--- course2/sem3/hw1/task1/task1/Node.cs
+++ course2/sem3/hw1/task1/task1/Node.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace task1
 {
-    class Node
+    public partial class BinaryTree<T>
+        where T : IComparable
     {
+        public class Node<T>
+        {
+            public T data { get; }
+            public Node<T> left { get; set; }
+            public Node<T> right { get; set; }
+            public Node(T data)
+            {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
     }
 }
