1b38c1d5e6a5741b9ce61b2fbff4033a81fc9e3e
Paul Kizhnerov <stigma-diabolicum@yandex.by>
2017-11-08 19:52:03 +0300

    Created main window & draw area & undo/redo buttons

---
+++ course2/sem3/hw1/task4/task4/SimpleRedactor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task4
+{
+    public partial class SimpleRedactor : Form
+    {
+        public SimpleRedactor()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void UndoButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
