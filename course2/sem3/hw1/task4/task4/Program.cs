8d2b8280f23e3e029e82cb1db604b83ec887d5e7
Paul Kizhnerov <stigma-diabolicum@yandex.by>
2017-12-17 02:56:48 +0300

    Deletes useless usings

--- course2/sem3/hw1/task4/task4/Program.cs
+++ course2/sem3/hw1/task4/task4/Program.cs
@@ -1,9 +1,6 @@
 ﻿namespace Task4
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     public static class Program
