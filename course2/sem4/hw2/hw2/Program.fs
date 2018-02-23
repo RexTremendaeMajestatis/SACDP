7228190179971d5a384efe40f82e38456a4c6051
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-02-24 00:17:54 +0300

    fix

--- course2/sem4/hw2/hw2/Program.fs
+++ course2/sem4/hw2/hw2/Program.fs
@@ -1,10 +1,10 @@
 ﻿(*2.1*)
 
-let count n = 
-    let rec recCount n acc = 
-        if n = 0 then acc
-        else acc * recCount (n / 10) (n % 10)
-    recCount n 1
+let count2 n = 
+    let rec recCount2 n b = 
+        if n < 10 then n * b
+        else recCount2 (n / 10) (b * (n % 10))
+    recCount2 n 1
 
 (*2.2*)
 
@@ -14,11 +14,15 @@ let indexOf element list =
         | [] -> -1
         | head :: tail -> if head = element then index
                           else recIndexOf (index + 1) tail
-    recIndexOf 0 list
+    if recIndexOf 0 list = -1 then None
+    else Some(recIndexOf 0 list)
 
 (*2.3*)
 
 let palindrom (x:string) = 
     match x with
     | "" -> true
-    | _ -> Seq.forall(fun i -> x.[i] = x.[x.Length - i - 1]) {0..x.Length/2}
+    | _ -> Seq.forall(fun i -> x.[i] = x.[x.Length - i - 1]) {0..x.Length / 2}
+
+(*2.4*)
+
