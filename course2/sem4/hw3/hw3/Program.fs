a0af4e5f22c88a8a50538a17a1d7feb6d7d5e964
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-02-27 23:13:35 +0300

    3.1 has done without tests

---
+++ course2/sem4/hw3/hw3/Program.fs
@@ -0,0 +1,18 @@
+﻿(*3.1.1*)
+
+let countOddByFold list = 
+    List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list
+
+(*3.1.2*)
+
+let countOddByFilter list = 
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.length
+
+(*3.1.3*)
+
+let countOddByMap list = 
+    list
+    |> List.map (fun x -> (x + 1) % 2)
+    |> List.fold (fun acc x -> x + acc) 0
