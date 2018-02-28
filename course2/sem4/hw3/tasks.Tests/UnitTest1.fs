f7adb8523d9631c380d358a049ea9a86b9284ab0
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-02-28 15:54:11 +0300

    3.1 has done with NUnit

--- course2/sem4/hw3/tasks.Tests/UnitTest1.fs
+++ course2/sem4/hw3/tasks.Tests/UnitTest1.fs
@@ -1,14 +1,76 @@
-namespace tasks.Tests.Tests
+module Tests
 
 open NUnit.Framework
+open tasks
 
-[<TestClass>]
-type TestClass () =
+[<Test>]
+let ``Empty filter test`` () = 
+    let expected = 0
+    let actual = task1.countOddByFilter []
+    Assert.That(actual, Is.EqualTo(expected))
 
-    [<SetUp>]
-    member this.Setup () =
-        ()
+[<Test>]
+let ``Simple filter test`` () = 
+    let expected = 4
+    let actual = task1.countOddByFilter [1; 2; 3; 4; 5; 6; 7; 8]
+    Assert.That(actual, Is.EqualTo(expected))
 
-    [<Test>]
-    member this.Test1 () =
-        Assert.Pass()
+[<Test>]
+let ``Advanced filter test`` () = 
+    let expected = 50
+    let actual = task1.countOddByFilter [1..100]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Zero odd numbers filter test`` () =
+    let expected = 0
+    let actual = task1.countOddByFilter [1; 3; 5; 7]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Empty fold test`` () = 
+    let expected = 0
+    let actual = task1.countOddByFold []
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Simple fold test`` () = 
+    let expected = 4
+    let actual = task1.countOddByFold [1; 2; 3; 4; 5; 6; 7; 8]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Advanced fold test`` () = 
+    let expected = 50
+    let actual = task1.countOddByFold [1..100]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Zero odd numbers fold test`` () =
+    let expected = 0
+    let actual = task1.countOddByFold [1; 3; 5; 7]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Empty map test`` () = 
+    let expected = 0
+    let actual = task1.countOddByMap []
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Simple map test`` () = 
+    let expected = 4
+    let actual = task1.countOddByMap [1; 2; 3; 4; 5; 6; 7; 8]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Advanced map test`` () = 
+    let expected = 50
+    let actual = task1.countOddByMap [1..100]
+    Assert.That(actual, Is.EqualTo(expected))
+
+[<Test>]
+let ``Zero odd numbers map test`` () =
+    let expected = 0
+    let actual = task1.countOddByMap [1; 3; 5; 7]
+    Assert.That(actual, Is.EqualTo(expected))
--- course2/sem4/hw3/tasks/Library.fs
+++ course2/sem4/hw3/tasks/Library.fs
@@ -1,5 +1,22 @@
 namespace tasks
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module task1 =
+    (*3.1.1*)
+
+    let countOddByFold list = 
+         List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list
+
+    (*3.1.2*)
+
+    let countOddByFilter list = 
+        list
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.length
+
+    (*3.1.3*)
+
+    let countOddByMap list = 
+        list
+        |> List.map (fun x -> (x + 1) % 2)
+        |> List.fold (fun acc x -> x + acc) 0
+        
