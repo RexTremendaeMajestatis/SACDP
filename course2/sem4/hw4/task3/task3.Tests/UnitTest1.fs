e0a41c423ed2439d3d63a5da4caeabef82cddbbf
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-03-10 00:28:01 +0300

    4.3 70% has done

--- course2/sem4/hw4/task3/task3.Tests/UnitTest1.fs
+++ course2/sem4/hw4/task3/task3.Tests/UnitTest1.fs
@@ -1,14 +1,70 @@
-namespace task3.Tests.Tests
+module Tests
 
 open NUnit.Framework
+open task3.task3
 
-[<TestClass>]
-type TestClass () =
+[<Test>]
+let ``Simple FV test1.1`` () = 
+    let expected = ['x']
+    let actual = getFV (Var('x'))
+    Assert.That(expected, Is.EqualTo(actual))
 
-    [<SetUp>]
-    member this.Setup () =
-        ()
+[<Test>]
+let ``Simple FV test1.2`` () = 
+    let expected = ['x']
+    let actual = getFV (Var('y'))
+    Assert.IsFalse(expected.Equals(actual))
 
-    [<Test>]
-    member this.Test1 () =
-        Assert.Pass()
+[<Test>]
+let ``Simple FV test2.1`` () = 
+    let expected = ['x'; 'y']
+    let actual = getFV (App(Var('x'), Var('y')))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Simple FV test3.1`` () = 
+    let expected = ['x']
+    let actual = getFV (Abs('y', Var('x')))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Simple FV test3.2`` () = 
+    let expected = []
+    let actual = getFV (Abs('x', Var('x')))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Advanced FV test1`` () = 
+    let expected = ['z'; 'n']
+    let actual = getFV (App(Abs('x', Var('z')), Abs('y', Var('n'))))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Advanced FV test2`` () = 
+    let expected = ['b']
+    let actual = getFV (App(Abs('x', Var('x')), Abs('a', Var('b'))))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Advanced FV test3`` () = 
+    let expected = []
+    let actual = getFV (App(Abs('x', Var('x')), Abs('a', Var('a'))))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Advanced FV test4`` () = 
+    let expected = ['x']
+    let actual = getFV (App(Abs('x', Var('x')), Var('x')))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Advanced FV test5`` () = 
+    let expected = ['y'; 'x']
+    let actual = getFV (App(Abs('x', Var('y')), Abs('y', Var('x'))))
+    Assert.That(expected, Is.EqualTo(actual))
+
+[<Test>]
+let ``Presentation example test`` () = 
+    let expected = ['z']
+    let actual = getFV (App(Abs('x', Abs('y', Var('x'))), Abs('x', App(Var('z'), Var('x')))))
+    Assert.That(expected, Is.EqualTo(actual))
--- course2/sem4/hw4/task3/task3/Library.fs
+++ course2/sem4/hw4/task3/task3/Library.fs
@@ -1,5 +1,31 @@
 namespace task3
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module task3 = 
+    
+    type Term = 
+        | Var of char
+        | App of Term * Term
+        | Abs of char * Term
+
+    // Get list of free variables in term
+    let getFV (t: Term) = 
+        let rec recGetFV t acc = 
+            match t with
+            | Var(x) -> x :: acc
+            | App(S, T) -> let left = recGetFV S acc
+                           let right = recGetFV T acc
+                           List.append left right
+            | Abs(x, S) -> recGetFV S acc 
+                           |> List.filter (fun y -> y <> x)
+        recGetFV t []
+    
+    // S[x := T]
+    let rec substitude S x T =
+        match S with
+        | Var(y) -> if y = x then T
+                    else S
+        | App(S1, S2) -> App(substitude S1 x T, substitude S2 x T)
+        | Abs(y, S) -> match T with
+                       | Var(_) when y = x -> S
+                       | _ when (not (getFV T |> List.contains y)) || (not (getFV S |> List.contains x)) -> Abs(y, substitude S x T)
+                       | _ -> (*OOSTAL POMIRAU*)
