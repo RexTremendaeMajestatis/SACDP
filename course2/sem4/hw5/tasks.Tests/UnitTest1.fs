1b629b325d8ffbfde24216def251fb6d3e4be61c
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-03-14 00:09:57 +0300

    5.1 has done

--- course2/sem4/hw5/tasks.Tests/UnitTest1.fs
+++ course2/sem4/hw5/tasks.Tests/UnitTest1.fs
@@ -1,14 +1,32 @@
-namespace tasks.Tests.Tests
+module Tests
 
 open NUnit.Framework
+open tasks.task1
 
-[<TestClass>]
-type TestClass () =
+[<Test>]
+let ``Brackets test1`` () = 
+    let s =  "((((((((()()()()())))((((((((()))"
+    let expected = false
+    let actual = check s
+    Assert.AreEqual(expected, actual)
 
-    [<SetUp>]
-    member this.Setup () =
-        ()
+[<Test>]
+let ``Brackets test2`` () = 
+    let s = "(Кижнеров)[Павел]{Александрович}"
+    let expected = true
+    let actual = check s
+    Assert.AreEqual(expected, actual)
 
-    [<Test>]
-    member this.Test1 () =
-        Assert.Pass()
+[<Test>]
+let ``Brackets test3`` () = 
+    let s = "Кижнеров Павел Александрович"
+    let expected = true
+    let actual = check s
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Brackets test4`` () = 
+    let s = ""
+    let expected = true
+    let actual = check s
+    Assert.AreEqual(expected, actual)
--- course2/sem4/hw5/tasks/Library.fs
+++ course2/sem4/hw5/tasks/Library.fs
@@ -1,5 +1,18 @@
 namespace tasks
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module task1 = 
+    (*пустая строка - правильная скобочная последовательность*)
+    (*последовательность без скобок - правильная скобочная последовательность*)
+    let check (s: string) =
+        let list = (s.ToCharArray()) |> List.ofArray
+
+        let rec checkRec list buffer =
+            match list with
+            | [] -> List.empty = buffer
+            | head :: tail -> match head with
+                            | _ when (head = '(') || (head = '[') || (head = '{') -> checkRec tail (head :: buffer)
+                            | ')' -> if buffer.Head = '(' then checkRec tail buffer.Tail else false
+                            | ']' -> if buffer.Head = '[' then checkRec tail buffer.Tail else false
+                            | '}' -> if buffer.Head = '{' then checkRec tail buffer.Tail else false
+                            | _ -> checkRec tail buffer
+        checkRec list []
