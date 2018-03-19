4ec6b43dc2d192eaca7d3657d910b856f21e3568
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-03-20 00:38:42 +0300

    5.3 has done

--- course2/sem4/hw5/tasks/task3.fs
+++ course2/sem4/hw5/tasks/task3.fs
@@ -2,7 +2,11 @@
 
 module task3 = 
 
-    type record = {name: string; phoneNumber: string}
+    type record (name: string, phoneNumber: string) = 
+        member this.name = name
+        member this.phoneNumber = phoneNumber
+        override this.ToString() = 
+            this.name + " " + this.phoneNumber
 
     let rec findByName (name: string) (list: list<record>) = 
         match list with
@@ -22,26 +26,59 @@ module task3 =
                           showAllData tail
         | [] -> printfn "End of database"
 
-    let rec dic (list: list<record>) = 
+    let save (records: list<record>) (path: string) = 
+        let recordsToString (records: list<record>) = 
+            let rec recordsToStringRec (records: list<record>) strList =
+                match records with
+                | head :: tail -> recordsToStringRec tail (head.ToString() :: strList)
+                | [] -> strList
+            recordsToStringRec records []
+        let recordsList = recordsToString records
+        System.IO.File.WriteAllLines(path, recordsList)
+    
+    let load (path: string) = 
+        if  System.IO.File.Exists path then
+            let records = List.ofArray (System.IO.File.ReadAllLines(path))
+            let recordsFromString (strList: List<string>) =
+                let rec recordsFromStringRec recordsList (strList: List<string>) = 
+                    match strList with
+                    | head :: tail -> let split = head.Split(' ')
+                                      recordsFromStringRec (record(split.[0], split.[1]) :: recordsList) tail
+                    | [] -> recordsList
+                recordsFromStringRec [] strList
+            recordsFromString records
+        else []
+
+    let rec phoneBook (records: list<record>) = 
         printfn "Enter command"
         let command = System.Console.ReadLine()
         match command with
-        | "2" -> printfn "Add new record\nEnter name: "
-                 let tempName = System.Console.ReadLine()
+        | "1" -> printfn "Shut down *sad music*"
+        | "2" -> printfn "Add a new record...\nEnter name: "
+                 let name = System.Console.ReadLine()
                  printfn "Enter phone number: "
-                 let tempPhoneNumber = System.Console.ReadLine()
-                 let tempRecord = {name = tempName; phoneNumber = tempPhoneNumber}
-                 dic (tempRecord :: list)
-        | "3" -> printfn "Find phone number by name"
-                 printfn "Enter name: "
+                 let phoneNumber = System.Console.ReadLine()
+                 let tempRecord = record(name, phoneNumber)
+                 phoneBook (tempRecord :: records)
+        | "3" -> printfn "Find phone number by name...\nEnter name: "
                  let tempName = System.Console.ReadLine()
-                 findByName tempName list
-                 dic list
-        | "4" -> printfn "Find name by phone number"
-                 printfn "Enter phone number: "
+                 findByName tempName records
+                 phoneBook records
+        | "4" -> printfn "Find name by phone number...\nEnter phone number: "
                  let tempNumber = System.Console.ReadLine()
-                 findByNumber tempNumber list
-                 dic list 
-        | "5" -> showAllData list
-                 dic list
-        | _ -> printfn "fff"
+                 findByNumber tempNumber records
+                 phoneBook records 
+        | "5" -> showAllData records
+                 phoneBook records
+        | "6" -> printfn "Saving the database to file...\nEnter the target path: "
+                 let path = System.Console.ReadLine()
+                 save records path
+                 printfn "Saving complete"
+                 phoneBook records
+        | "7" -> printfn "Loading database from file...\nEnter the target path: "
+                 let path = System.Console.ReadLine()
+                 let loadedRecords = load path
+                 printfn "Loading complete"
+                 phoneBook loadedRecords
+        | _ -> printfn "Unknown command"
+               phoneBook records
