faf66c6fa16f8fb6da6b7cfc687ee7d0d37917cb
RexTremendaeMajestatis <stigma-diabolicum@yandex.by>
2018-04-04 03:34:05 +0300

    5.2 fixed

--- course2/sem4/hw5/tasks/task1.fs
+++ course2/sem4/hw5/tasks/task1.fs
@@ -6,13 +6,18 @@ module task1 =
     let check (s: string) =
         let list = (s.ToCharArray()) |> List.ofArray
 
+        let checker head (buffer: char list) = 
+            match head with
+            | ')' -> buffer.Head = '('
+            | ']' -> buffer.Head = '['
+            | '}' -> buffer.Head = '{'
+            | _ -> false
+
         let rec checkRec list buffer =
             match list with
             | [] -> List.empty = buffer
             | head :: tail -> match head with
                               | _ when (head = '(') || (head = '[') || (head = '{') -> checkRec tail (head :: buffer)
-                              | ')' -> if buffer.Head = '(' then checkRec tail buffer.Tail else false
-                              | ']' -> if buffer.Head = '[' then checkRec tail buffer.Tail else false
-                              | '}' -> if buffer.Head = '{' then checkRec tail buffer.Tail else false
+                              | ')' | ']' | '}' -> if checker head buffer then checkRec tail buffer.Tail else false
                               | _ -> checkRec tail buffer
         checkRec list []
